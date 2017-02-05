bce60f49684040ceb3bca94018edaca10b4dcaf2
BenRamsden <benramsdensii@gmail.com>
2017-02-05 15:44:18 +0000

    Debugging missing POST endpoint

--- NavigationServer/routes/observations.js
+++ NavigationServer/routes/observations.js
@@ -57,6 +57,7 @@ router.get('/:obtype', function(req, res, next) {
                 processAccelerometerObservation(idUser, req, processCallback);
                 break;
             default:
+                console.log("Hit default with obtype: "+obtype);
                 break;
         }
 
