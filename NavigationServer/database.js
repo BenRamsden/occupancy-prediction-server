d84e8185472d676e873f7d36915caaf80d90866b
BenRamsden <benramsdensii@gmail.com>
2017-02-19 20:54:37 +0000

    Returning all columns

--- NavigationServer/database.js
+++ NavigationServer/database.js
@@ -149,7 +149,7 @@ database.prototype.getOccupancyEstimation = function(apitoken, lat, lng, callbac
 
     //var example = "SELECT id, ( 3959 * acos( cos( radians(37) ) * cos( radians( lat ) ) * cos( radians( lng ) - radians(-122) ) + sin( radians(37) ) * sin( radians( lat ) ) ) ) AS distance FROM markers HAVING distance < 25 ORDER BY distance LIMIT 0 , 20;";
 
-    var query_0 = "SELECT idHotspotObservation ," +
+    var query_0 = "SELECT * ," +
         " ( 3959" +
         " * acos( cos( radians( ? ) )" +
         " * cos( radians( lat ) )" +
