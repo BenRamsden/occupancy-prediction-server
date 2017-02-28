356357aa7f6c35bb89eb6fdf6327b74a2d2f7e69
BenRamsden <benramsdensii@gmail.com>
2017-02-28 21:57:04 +0000

    Made server status 503 if handleError sent error

--- NavigationServer/handleError.js
+++ NavigationServer/handleError.js
@@ -5,6 +5,7 @@
 function handleError(temp_res, err_code) {
     console.log("HANDLE_ERROR: Sending error response, error: " + err_code);
 
+    temp_res.status(503);
     temp_res.json({ result : {success: false, reason: err_code }});
 
     return;
