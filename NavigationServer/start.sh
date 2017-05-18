b793c487cd9a39613791d02724f3a63aef32a8c9
Ubuntu <ubuntu@ip-172-31-24-214.eu-west-1.compute.internal>
2017-05-18 08:44:20 +0000

    Added firewall redirect 80 -> 8080 in start.sh, for uni firewall

--- NavigationServer/start.sh
+++ NavigationServer/start.sh
@@ -2,6 +2,12 @@
 
 #nohup DEBUG=navigationserver:* npm start > navigationserver_nohup.log 2>&1&
 
+#CLEAR NAT TABLE RULES
+sudo iptables -t nat -F
+
+#Route external port 80 requests to 8000
+sudo iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-ports 8080
+
 ps cax | grep node > /dev/null
 if [ $? -eq 0 ]; then
   echo "START: ERROR node process already running."
